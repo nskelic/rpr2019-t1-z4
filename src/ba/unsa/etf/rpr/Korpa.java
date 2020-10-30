3bde83a5b6d1ae594c6871f7e9d7032a095180b0
nskelic <nskelic1@etf.unsa.ba>
2020-10-30 17:25:31 +0100

    First commit

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,40 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] nizArtikala = new Artikl[50];
+    private int brojArtikala = 0;
+
+    public Korpa() {
+    }
+    public Artikl[] getArtikli() {
+        return nizArtikala;
+    }
+    public boolean dodajArtikl(Artikl artikl) {
+        if(brojArtikala==50) return false;
+        nizArtikala[brojArtikala++] = artikl;
+        return true;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl[] noviNiz = new Artikl[50];
+        Artikl artiklIzbacen = null;
+        int stariBroj = brojArtikala;
+        for(int i = 0, j = 0; i<stariBroj; i++) {
+            if(nizArtikala[i].getKod().equals(kod)) {
+                brojArtikala--;
+                artiklIzbacen = nizArtikala[i];
+                continue;
+            }
+            noviNiz[j] = nizArtikala[i];
+            j++;
+        }
+        nizArtikala = noviNiz;
+        return  artiklIzbacen;
+    }
+    public int dajUkupnuCijenuArtikala () {
+        int s=0;
+        for(int i = 0; i < brojArtikala; i++) {
+            s+=nizArtikala[i].getCijena();
+        }
+        return  s;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,39 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] nizArtikala = new Artikl[1000];
+    private int brojArtikala=0;
+
+    public Supermarket() {
+    }
+    public Artikl[] getArtikli() {
+        return nizArtikala;
+    }
+    public void dodajArtikl(Artikl artikl) {
+        if(brojArtikala==1000) return;
+        nizArtikala[brojArtikala++]=artikl;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl[] noviNiz = new Artikl[1000];
+        Artikl artiklIzbacen = null;
+        for(int i=0, j=0; i<brojArtikala; i++) {
+            if(nizArtikala[i].getKod().equals(kod)) {
+                brojArtikala--;
+                artiklIzbacen = nizArtikala[i];
+                continue;
+            }
+            noviNiz[j] = nizArtikala[i];
+            j++;
+        }
+        nizArtikala = noviNiz;
+        return artiklIzbacen;
+    }
+
+    public int dajUkupnuCijenuArtikala () {
+        int s=0;
+        for(int i=0; i<brojArtikala; i++) {
+            s+=nizArtikala[i].getCijena();
+        }
+        return s;
+    }
 }
